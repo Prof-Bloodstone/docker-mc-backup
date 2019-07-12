6479e0327a6c9b1555cd9ee2f0ddc8c30ffa3ff0
Professor Bloodstone <prof.bloodstone@gmail.com>
2019-07-12 19:23:27 +0200

    Add PoC log function, that can handle stdin data as well, fix file exclusion, minor fixes

--- backup-loop.sh
+++ backup-loop.sh
@@ -2,12 +2,30 @@
 
 set -eu
 
+set -x # TODO: REMOVE ME!
+# TODO: REMOVE ME!!!
+rcon-cli() {
+  true
+}
+
+poc_log() {
+  if [ "$#" -lt 1 ]; then
+    echo "Wrong number of arguments passed to poc_log function" >&2
+    exit 1
+  fi
+  local level="${1}"
+  shift
+  logs="${*:-"$(cat -)"}"
+
+  <<<"${logs}" awk -v level="${level}" '/^[[:space:]]*$/ {print $0; next;} { "date -Iseconds" | getline d; printf("%s %s %s\n", d, level, $0); close("date")}'
+}
+
 log() {
   if [ "$#" -lt 2 ]; then
     echo "Wrong number of arguments passed to log function" >&2
     exit 1
   fi
-  local level=${1}
+  local level="${1}"
   shift
   echo "$(date -Iseconds) ${level} $*"
 }
@@ -16,51 +34,41 @@ find_old_backups() {
   find "${DEST_DIR}" -mtime "+${PRUNE_BACKUPS_DAYS}" "${@}"
 }
 
-backupSet="${BACKUP_SET:-}"
-# shellcheck disable=SC2089
-excludes="${EXCLUDES:-"--exclude '*.jar'"}"
-
 : "${SRC_DIR:=/data}"
 : "${DEST_DIR:=/backups}"
 : "${BACKUP_NAME:=world}"
-: "${INITIAL_DELAY:=120}"
-: "${INTERVAL_SEC:=86400}"
+: "${INITIAL_DELAY:=2*60}" # 2*60 seconds = 2 minutes
+: "${INTERVAL_SEC:=24*60*60}" # 24*60*60 = 24 hours
 : "${PRUNE_BACKUPS_DAYS:=7}"
-: "${TYPE:=VANILLA}"
+: "${TYPE:=VANILLA}" # unused
 : "${RCON_PORT:=25575}"
 : "${RCON_PASSWORD:=minecraft}"
+: "${EXCLUDES:=*.jar,cache,logs}" # Comma separated list of glob(3) patterns
 
-case "${TYPE}" in
-  FTB|CURSEFORGE)
-    cd "${SRC_DIR}/FeedTheBeast"
-    ;;
-  BUKKIT|SPIGOT|PAPER)
-    cd "${SRC_DIR}"
-    if [ -z "${LEVEL:-}" ]; then
-      LEVEL="world world_nether world_the_end"
-    fi
-    ;;
-  *)
-    cd "${SRC_DIR}"
-    ;;
-esac
+# We unfortunately can't use a here-string, as it inserts new line at the end
+readarray -td, excludes_patterns < <(printf '%s' "${EXCLUDES}")
 
-: "${LEVEL:=world}"
+excludes=()
+for pattern in "${excludes_patterns[@]}"; do
+  excludes+=(--exclude "${pattern}")
+done
+
+# Should we even do this?
+ftb_dir="${SRC_DIR}/FeedTheBeast"
+if [ -d "${ftb_dir}" ]; then
+  SRC_DIR="${ftb_dir}"
+fi
 
 log INFO "waiting initial delay of ${INITIAL_DELAY} seconds..."
 sleep "$(( INITIAL_DELAY ))"
 
-backupSet="${backupSet} ${LEVEL}"
-backupSet="${backupSet} $(find . -maxdepth 1 -name '*.properties' -o -name '*.yml' -o -name '*.yaml' -o -name '*.json' -o -name '*.txt')"
-
-if [ -d plugins ]; then
-  backupSet="${backupSet} plugins"
-fi
-
 log INFO "waiting for rcon readiness..."
 while true; do
-  rcon-cli save-on >& /dev/null && break
-  sleep 10
+  if rcon-cli save-on &> /dev/null; then
+    break
+  else
+    sleep 10
+  fi
 done
 
 while true; do
@@ -70,12 +78,12 @@ while true; do
     if rcon-cli save-all; then
 
       outFile="${DEST_DIR}/${BACKUP_NAME}-${ts}.tgz"
-      log INFO "backing up content in $(pwd) to ${outFile}"
+      log INFO "backing up content in ${SRC_DIR} to ${outFile}"
       # shellcheck disable=SC2086
-      if tar cz -f "${outFile}" ${backupSet} ${excludes}; then
-        log ERROR "backup failed"
-      else
+      if tar "${excludes[@]}" -czf "${outFile}" -C "${SRC_DIR}" .; then
         log INFO "successfully backed up"
+      else
+        log ERROR "backup failed"
       fi
 
     fi
@@ -86,7 +94,7 @@ while true; do
 
   if (( PRUNE_BACKUPS_DAYS > 0 )) && [ -n "$(find_old_backups -print -quit)" ]; then
     log INFO "pruning backup files older than ${PRUNE_BACKUPS_DAYS} days"
-    find_old_backups -print -delete
+    find_old_backups -print -delete | poc_log INFO
   fi
 
   log INFO "sleeping ${INTERVAL_SEC} seconds..."
